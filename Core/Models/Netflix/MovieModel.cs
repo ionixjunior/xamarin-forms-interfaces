e5db8b17997a461efa1d3001f538a76705e630da
Ione Souza Junior <junior@ionixjunior.com.br>
2018-05-24 22:55:54 -0300

    Adicionado model do netflix

---
+++ Core/Models/Netflix/MovieModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Core.Models.Netflix
+{
+    public class MovieModel
+    {
+        public string Photo { get; set; }
+        public string Category { get; set; }
+    }
+}
