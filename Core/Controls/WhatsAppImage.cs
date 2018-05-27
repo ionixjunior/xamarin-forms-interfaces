882a7ff292e92ebdbc77825d6bf669769683e0cf
Ione Souza Junior <junior@ionixjunior.com.br>
2018-05-27 13:23:18 -0300

    Simplificado tela do whatsapp

---
+++ Core/Controls/WhatsAppImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FFImageLoading.Forms;
+using FFImageLoading.Transformations;
+
+namespace Core.Controls
+{
+    public class WhatsAppImage : CachedImage
+    {
+        public WhatsAppImage()
+        {
+            WidthRequest = 60;
+            HeightRequest = 60;
+            DownsampleToViewSize = true;
+
+            Transformations.Add(new CircleTransformation());
+        }
+    }
+}
