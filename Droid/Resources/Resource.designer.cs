6997949231e53c4b5fca0c6cfe9615067d80c160
Ione Souza Junior <junior@ionixjunior.com.br>
2018-05-29 22:22:50 -0300

    Microcharts

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -26,6 +26,7 @@ namespace Core.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Microcharts.Droid.Resource.String.library_name = global::Core.Droid.Resource.String.library_name;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::Core.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -3726,6 +3727,9 @@ namespace Core.Droid
 			
 			// aapt resource value: 0x7f090038
 			public const int character_counter_pattern = 2131296312;
+			
+			// aapt resource value: 0x7f09003e
+			public const int library_name = 2131296318;
 			
 			// aapt resource value: 0x7f090000
 			public const int mr_button_content_description = 2131296256;
