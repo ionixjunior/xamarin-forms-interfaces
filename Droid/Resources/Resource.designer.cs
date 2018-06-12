0048ce146659d4c7eb73d89b3b7bfe6a519335e7
Ione Souza Junior <junior@ionixjunior.com.br>
2018-06-11 21:39:32 -0300

    Adicionado imagens no Android

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -4481,26 +4481,26 @@ namespace Core.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020112
-			public const int avd_hide_password_1 = 2130837778;
+			// aapt resource value: 0x7f02011e
+			public const int avd_hide_password_1 = 2130837790;
 			
-			// aapt resource value: 0x7f020113
-			public const int avd_hide_password_2 = 2130837779;
+			// aapt resource value: 0x7f02011f
+			public const int avd_hide_password_2 = 2130837791;
 			
-			// aapt resource value: 0x7f020114
-			public const int avd_hide_password_3 = 2130837780;
+			// aapt resource value: 0x7f020120
+			public const int avd_hide_password_3 = 2130837792;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020115
-			public const int avd_show_password_1 = 2130837781;
+			// aapt resource value: 0x7f020121
+			public const int avd_show_password_1 = 2130837793;
 			
-			// aapt resource value: 0x7f020116
-			public const int avd_show_password_2 = 2130837782;
+			// aapt resource value: 0x7f020122
+			public const int avd_show_password_2 = 2130837794;
 			
-			// aapt resource value: 0x7f020117
-			public const int avd_show_password_3 = 2130837783;
+			// aapt resource value: 0x7f020123
+			public const int avd_show_password_3 = 2130837795;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -5051,11 +5051,11 @@ namespace Core.Droid
 			// aapt resource value: 0x7f02010b
 			public const int notification_icon_background = 2130837771;
 			
-			// aapt resource value: 0x7f020110
-			public const int notification_template_icon_bg = 2130837776;
+			// aapt resource value: 0x7f02011c
+			public const int notification_template_icon_bg = 2130837788;
 			
-			// aapt resource value: 0x7f020111
-			public const int notification_template_icon_low_bg = 2130837777;
+			// aapt resource value: 0x7f02011d
+			public const int notification_template_icon_low_bg = 2130837789;
 			
 			// aapt resource value: 0x7f02010c
 			public const int notification_tile_bg = 2130837772;
@@ -5064,10 +5064,46 @@ namespace Core.Droid
 			public const int notify_panel_notification_icon_bg = 2130837773;
 			
 			// aapt resource value: 0x7f02010e
-			public const int Prev = 2130837774;
+			public const int nubank_cat_bullet_green = 2130837774;
 			
 			// aapt resource value: 0x7f02010f
-			public const int Up = 2130837775;
+			public const int nubank_cat_bullet_red = 2130837775;
+			
+			// aapt resource value: 0x7f020110
+			public const int nubank_cat_card = 2130837776;
+			
+			// aapt resource value: 0x7f020111
+			public const int nubank_cat_eletronics = 2130837777;
+			
+			// aapt resource value: 0x7f020112
+			public const int nubank_cat_play = 2130837778;
+			
+			// aapt resource value: 0x7f020113
+			public const int nubank_cat_recreation = 2130837779;
+			
+			// aapt resource value: 0x7f020114
+			public const int nubank_cat_restaurant = 2130837780;
+			
+			// aapt resource value: 0x7f020115
+			public const int nubank_cat_services = 2130837781;
+			
+			// aapt resource value: 0x7f020116
+			public const int nubank_cat_star = 2130837782;
+			
+			// aapt resource value: 0x7f020117
+			public const int nubank_cat_transport = 2130837783;
+			
+			// aapt resource value: 0x7f020118
+			public const int Prev = 2130837784;
+			
+			// aapt resource value: 0x7f020119
+			public const int Up = 2130837785;
+			
+			// aapt resource value: 0x7f02011a
+			public const int whatsapp_mute = 2130837786;
+			
+			// aapt resource value: 0x7f02011b
+			public const int whatsapp_read = 2130837787;
 			
 			static Drawable()
 			{
